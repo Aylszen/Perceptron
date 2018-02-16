8f51c50274ea6a7da3a311b11989db7a5258e1fc
Aylszen <aylszen@gmail.com>
2018-02-16 19:28:44 +0100

    Whole functionality added

--- src/Perceptron.java
+++ src/Perceptron.java
@@ -1,26 +1,15 @@
 import java.util.Random;
 
 public class Perceptron {
-	double w1, w2, b, x, y, a;
-	final int M = 10, N = 2;
-	int[][] p = new int[10][N];
-	int[] t = new int[M];
+	Double w1, w2, b, x, y, a; 
+	Integer e;
 
 	public Perceptron() {
-		p[0][0] = -9;
-		p[0][0] = -10;
-		p[1][0] = -8;
-		p[1][1] = 20;
-		p[2][0] = -6;
-		p[2][1] = -5;
-		p[3][0] = -6;
-		p[3][1] = -25;
-		// JESCZE TROCHE TRZEBA DODAC;
-
 		Random rand = new Random();
 		w1 = rand.nextDouble();
 		w2 = rand.nextDouble();
 		b = rand.nextDouble();
+		e = 0;
 	}
 
 	public void calculateSignalValue(int x, int y) {
@@ -28,9 +17,43 @@ public class Perceptron {
 		System.out.println("a = " + a);
 	}
 
-	public void execute() {
-		for (int i = 0; i < 3; i++) {
+	public int functionOfActivation() {
+		if (a > 0) {
+			return 1;
+		} else {
+			return 0;
+		}
+	}
+
+	public void calculateError(int i, int t) {
+		e = t - functionOfActivation();
+		System.out.println("e = " + e);
+	}
+
+	public void setNewWagesAndShift(int i, int x, int y)
+	{
+		w1 = w1 + e * x;
+		w2 = w2 + e * y;
+		b = b + e;
+	}
+	
+	public void checkIfCorrect(int t)
+	{
+		if (t == functionOfActivation())
+		{
+			System.out.println("Uda³o siê :)");
+		}
+	}
+	
+	public void execute(int[][] p, int M, int[] t) {
+		for (int i = 0; i < M; i++) {
 			calculateSignalValue(p[i][0], p[i][1]);
+			checkIfCorrect(t[i]);
+			calculateError(i,t[i]);
+			if (e!=0)
+			{
+				setNewWagesAndShift(i,p[i][0], p[i][1]);
+			}
 		}
 	}
 
--- src/Project.java
+++ src/Project.java
@@ -2,8 +2,46 @@
 public class Project {
 
 	public static void main(String[] args) {
+		final Integer M = 10, N = 2;
+		int[][] p = new int[10][N];
+		int[] t = new int[M];
 		Perceptron perceptron = new Perceptron();
-		perceptron.execute();
+		
+		//****UCZENIE******//
+		p[0][0] = -9;
+		p[0][0] = -10;
+		p[1][0] = -8;
+		p[1][1] = 20;
+		p[2][0] = -6;
+		p[2][1] = -5;
+		p[3][0] = -6;
+		p[3][1] = -25;
+		p[4][0] = -2;
+		p[4][1] = -10;
+		p[5][0] = 1;
+		p[5][1] = -10;
+		p[6][0] = 2;
+		p[6][1] = 20;
+		p[7][0] = 4;
+		p[7][1] = -16;
+		p[8][0] = 4;
+		p[8][1] = 25;
+		p[9][0] = 5;
+		p[9][1] = 9;
+
+		t[0] = 1;
+		t[1] = 1;
+		t[2] = 1;
+		t[3] = 0;
+		t[4] = 0;
+		t[5] = 0;
+		t[6] = 1;
+		t[7] = 0;
+		t[8] = 1;
+		t[9] = 0;
+
+		perceptron.execute(p,M,t);
+		//****UCZENIE******//
 	}
 
 }
